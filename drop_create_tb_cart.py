d8c6ceb57f4baada2649aaed0f54a49537f43cc7
miguelmmaraujo01 <51483730+miguelmmaraujo01@users.noreply.github.com>
2020-06-11 16:21:55 -0300

    Alteracao retorno msgm log info

--- drop_create_tb_cart.py
+++ drop_create_tb_cart.py
@@ -20,13 +20,13 @@ def drop_create_tb_cart():
         try:
             cursor.execute('drop table credito.tmp_dbm_credito_pf')
         except Exception as err:
-            log.logger.info('table credito.tmp_dbm_credito_pf idx_tmpdbcredpf_01 not exists')#print('idx tmp_dbm_credito_pf nao existe')
+            log.logger.info('table credito.tmp_dbm_credito_pf not exists')#print('idx tmp_dbm_credito_pf nao existe')
         pass
 
         cursor.execute(""" create table credito.tmp_dbm_credito_pf as select documento as originador, anomes, translate( upper(carteira),'ÁÇÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕËÜáçéíóúàèìòùâêîôûãõëü','ACEIOUAEIOUAEIOUAOEUaceiouaeiouaeiouaoeu') as carteira from credito.dbm_credito_massa where carteira is not null""")
         cursor.execute('create index idx_tmpdbcredpf_01 on credito.tmp_dbm_credito_pf (originador) tablespace tsicredito')
     
-        log.logger.info('object credito.tmp_dbm_credito_pf - idx_tmpdbcredpf_01 created')#print('tabela tmp_dbm_credito_pf criada')
+        log.logger.info('objects credito.tmp_dbm_credito_pf - idx_tmpdbcredpf_01 created')#print('tabela tmp_dbm_credito_pf criada')
         
         try:
             cursor.execute('drop index credito.idx_tmpdbcredpj_01')
@@ -43,7 +43,7 @@ def drop_create_tb_cart():
         cursor.execute("""create table credito.tmp_dbm_credito_pj as select documento as originador, anomes, translate(upper(carteira),'ÁÇÉÍÓÚÀÈÌÒÙÂÊÎÔÛÃÕËÜáçéíóúàèìòùâêîôûãõëü','ACEIOUAEIOUAEIOUAOEUaceiouaeiouaeiouaoeu') as carteira from credito.dbm_credito_massa_pj where carteira is not null""")
         cursor.execute('create index idx_tmpdbcredpj_01 on credito.tmp_dbm_credito_pj (originador) tablespace tsicredito')
         
-        log.logger.info('table credito.tmp_dbm_credito_pj - idx_tmpdbcredpj_01 created')#print('tabela tmp_dbm_credito_pf criada')
+        log.logger.info('objects credito.tmp_dbm_credito_pj - idx_tmpdbcredpj_01 created')#print('tabela tmp_dbm_credito_pf criada')
         #os.system("pause")
 
     except Exception as err:
