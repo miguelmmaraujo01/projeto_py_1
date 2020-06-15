c3db180a5d74aabb53da55bcde52f2b88f2f3e11
miguelmmaraujo01 <51483730+miguelmmaraujo01@users.noreply.github.com>
2020-06-15 20:54:54 -0300

    Criado controle execucao e if de validacao stop processo 1 fluxo der erro

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pag_status_carteira as etapa3
 
 import p2p_pag_loader_daleyd_arq as p2p_etapa1
 import p2p_pag_carteira_aging as p2p_etapa2
+import control_exec as ctrl
 
 # endponint receber o nome do arq ou chave que o s3 gera par ao arquivo, arquivo deve ter nomes dif
 identificador_arquivo = 'delayed-receivables'
@@ -13,15 +14,47 @@ identificador_arquivo = 'delayed-receivables'
 def ordem_exec(param_ind_arquivo):
     try:
         if (param_ind_arquivo.count("biva")) or (param_ind_arquivo.count("p2p")):
-            print(param_ind_arquivo)
+
             p2p_etapa1.p2p_pag_loader_delayd_receivables_arq(identificador_arquivo)
             p2p_etapa2.p2p_create_tb_carteira_aging(identificador_arquivo)
 
         elif (param_ind_arquivo.count("delayed")): #'delayed-receivables'
-            print(param_ind_arquivo)
-            #etapa1.pag_loader_delayd_receivables_arq(identificador_arquivo)
-            #etapa2.create_tb_carteira_aging(identificador_arquivo)
-            etapa3.create_tb_carteira_status()
+
+            #
+            ind_job = '1'
+            nam_job = 'pag_loader_daleyd_arq'
+            nam_etapa = 'etapa1'
+            nam_arq = '' 
+
+            ctrl.control_insert_exec(ind_job,nam_job,nam_etapa,nam_arq)
+            retorno = etapa1.pag_loader_delayd_receivables_arq(identificador_arquivo)
+            ctrl.control_update_exec(ind_job, nam_etapa, retorno[0], retorno[1])
+
+            if retorno[0] != 0:
+                raise SystemExit
+            else:
+
+                ##
+                ind_job = '2'
+                nam_job = 'pag_carteira_aging'
+                nam_etapa = 'etapa2'
+                nam_arq = ''   
+
+                ctrl.control_insert_exec(ind_job,nam_job,nam_etapa,nam_arq)
+                retorno = etapa2.create_tb_carteira_aging(identificador_arquivo)
+                ctrl.control_update_exec(ind_job, nam_etapa, retorno[0], retorno[1])
+
+                
+                ###
+                ind_job = '3'
+                nam_job = 'pag_status_carteira'
+                nam_etapa = 'etapa3'
+                nam_arq = '' 
+
+                ctrl.control_insert_exec(ind_job,nam_job,nam_etapa,nam_arq)
+                retorno = etapa3.create_tb_carteira_status()
+                ctrl.control_update_exec(ind_job, nam_etapa, retorno[0], retorno[1])
+
         else:
             log.logger.error('Name param invalid')
 
@@ -31,6 +64,3 @@ def ordem_exec(param_ind_arquivo):
 
 
 ordem_exec(identificador_arquivo)
-
-
-
