82b9b39056b7436d9d55fa34ad3fff7b9501a1bc
miguelmmaraujo01 <51483730+miguelmmaraujo01@users.noreply.github.com>
2020-06-16 17:39:16 -0300

    atualizacao para metrodo controle e exec de outro arquivo de outra regra de neg

--- main.py
+++ main.py
@@ -8,18 +8,38 @@ import p2p_pag_carteira_aging as p2p_etapa2
 import control_exec as ctrl
 
 # endponint receber o nome do arq ou chave que o s3 gera par ao arquivo, arquivo deve ter nomes dif
-identificador_arquivo = 'delayed-receivables'
+identificador_arquivo = 'p2p-delayed-receivables'
 #identificador_arquivo = 'biva'
 
 def ordem_exec(param_ind_arquivo):
     try:
-        if (param_ind_arquivo.count("biva")) or (param_ind_arquivo.count("p2p")):
+        if (param_ind_arquivo.count("biva")) or (param_ind_arquivo.count("p2p")):    
+            print(param_ind_arquivo)
+            #
+            ind_job = '4'
+            nam_job = 'p2p_pag_loader_daleyd_arq'
+            nam_etapa = 'p2p_etapa1'
+            nam_arq = '' 
 
-            p2p_etapa1.p2p_pag_loader_delayd_receivables_arq(identificador_arquivo)
-            p2p_etapa2.p2p_create_tb_carteira_aging(identificador_arquivo)
+            ctrl.control_insert_exec(ind_job,nam_job,nam_etapa,nam_arq)           
+            retorno = p2p_etapa1.p2p_pag_loader_delayd_receivables_arq(identificador_arquivo)
+            ctrl.control_update_exec(ind_job, nam_etapa, retorno[0], retorno[1])
 
-        elif (param_ind_arquivo.count("delayed")): #'delayed-receivables'
+            if retorno[0] != 0:
+                raise SystemExit
+            else:
 
+                #
+                ind_job = '5'
+                nam_job = 'p2p_pag_carteira_aging'
+                nam_etapa = 'p2p_etapa2'
+                nam_arq = '' 
+
+                ctrl.control_insert_exec(ind_job,nam_job,nam_etapa,nam_arq)               
+                retorno = p2p_etapa2.p2p_create_tb_carteira_aging(identificador_arquivo)
+                ctrl.control_update_exec(ind_job, nam_etapa, retorno[0], retorno[1])
+        
+        elif  (param_ind_arquivo.count("delayed")): #'delayed-receivables' 
             #
             ind_job = '1'
             nam_job = 'pag_loader_daleyd_arq'
